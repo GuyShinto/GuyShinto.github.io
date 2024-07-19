d224fa57ec5c84a233229f7549767ad10688f616
GuyShinto <xiiioffice000@gmail.com>
2024-07-19 21:44:21 +0700

    05

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
-function myFunction() { 
-    document.getElementById("demo").innerHTML = "alooo!";
-  }
+var text = document.getElementById('title-motion');
+var newDom = '';
+var animationDelay = 6;
+
+for(let i = 0; i < text.innerText.length; i++)
+{
+    newDom += '<span class="each-title-motion">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+}
+
+text.innerHTML = newDom;
+var length = text.children.length;
+
+for(let i = 0; i < length; i++)
+{
+    text.children[i].style['animation-delay'] = animationDelay * i * 15 + 'ms';
+}
